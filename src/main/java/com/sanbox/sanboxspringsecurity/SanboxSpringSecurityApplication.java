795307814b20ff1415a976139860b04b83a826e2
Evgen <evgen.990029@gmail.com>
2024-05-05 15:59:00 +0300

    варианты реализации точек входа

--- src/main/java/com/sanbox/sanboxspringsecurity/SanboxSpringSecurityApplication.java
+++ src/main/java/com/sanbox/sanboxspringsecurity/SanboxSpringSecurityApplication.java
@@ -3,11 +3,13 @@ package com.sanbox.sanboxspringsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -44,4 +46,83 @@ public class SanboxSpringSecurityApplication {
 				})
 				.build();
 	}
+
+	//точки входа
+
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http.authorizeHttpRequests()
+//				.anyRequest().authenticated().and()
+//				.build();
+//	}
+
+//	//переопределение точки входа
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http.authorizeHttpRequests()
+//				.anyRequest().authenticated().and()
+//				.exceptionHandling()
+//				.authenticationEntryPoint((request, response, authException) -> {
+//					authException.printStackTrace();
+//					response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//				}).and()
+//				.build();
+//	}
+
+	//перенаправление пользователя
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http
+//				.authorizeHttpRequests()
+//				.requestMatchers("/public/**").permitAll()
+//				.anyRequest().authenticated().and()
+//				.exceptionHandling()
+//				.authenticationEntryPoint((request, response, authException) -> {
+//					response.sendRedirect("http://localhost:8080/public/403.html");
+//				}).and()
+//				.build();
+//	}
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http
+//				.csrf().disable()
+//				.formLogin().and()
+//				.authorizeHttpRequests()
+//				.requestMatchers("/public/**").permitAll()
+//				.anyRequest().authenticated().and()
+//				.exceptionHandling()
+//				.authenticationEntryPoint((request, response, authException) -> {
+//					response.sendRedirect("http://localhost:8080/public/sign-in.html");
+//				}).and()
+//				.build();
+//	}
+
+	// Basic точка входа
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		return http
+//				.httpBasic().and()
+//				.authorizeHttpRequests()
+//					.anyRequest().authenticated().and()
+//				.build();
+//	}
+
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		BasicAuthenticationEntryPoint basicAuthenticationEntryPoint = new BasicAuthenticationEntryPoint();
+//		basicAuthenticationEntryPoint.setRealmName("Realm");
+//		return http
+//				.httpBasic().authenticationEntryPoint((request, response, authException) -> {
+//					authException.printStackTrace();
+//					basicAuthenticationEntryPoint.commence(request, response, authException);
+//				}).and()
+//				.authorizeHttpRequests()
+//					.anyRequest().authenticated().and()
+//				.exceptionHandling()
+//					.authenticationEntryPoint(basicAuthenticationEntryPoint)
+//					.and()
+//				.build();
+//	}
+
+
 }
